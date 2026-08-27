4b952273d05ca43a88ad2bb99f0cad628e97f04b
Godswill Nsude <josephnsudegodswill@gmail.com>
2026-08-27 15:28:43 -0500

    Remove duplicate Terraform backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
 terraform {
   required_version = ">= 0.12"
-  backend "s3" {
-    bucket = "josep-myapp-tf-state-2026"
-    key = "myapp/state.tfstate"
-    region = "us-east-2"
-  }
 }
 
 provider "aws" {
